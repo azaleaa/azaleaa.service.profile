143d91a2016021790811a8932b08cfd5341b5438
ramakrishnanjayaraman <ramakrishnan.jayaraman@gmail.com>
2017-03-02 07:41:54 +0000

    Added support for DB

--- api.go
+++ api.go
@@ -1,15 +1,72 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
 )
+
+type KeyValPair struct {
+	Key string `json:"key,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+type Profile struct {
+	Email string `json:"email,omitempty"`
+	Profilevals []KeyValPair `json:"profilevals,omitempty"`
+}
+
+type Configuration struct {
+	AZALEAA_DB_DSN string
+}
+
 
 func CreateProfileEndpoint(w http.ResponseWriter, req *http.Request) {
 //	params := mux.Vars(req)
-	fmt.Printf("Received request")
+	
+//	fmt.Println(req)
+
+	file, _ := os.Open("azaleaa.conf")
+	decoder1 := json.NewDecoder(file)
+	configuration := Configuration{}
+	err1 := decoder1.Decode(&configuration)
+
+	checkErr(err1)
+
+	var profile Profile 
+	decoder :=  json.NewDecoder(req.Body)
+
+	err := decoder.Decode(&profile)
+	
+	fmt.Println(profile)
+
+	db_dsn := configuration.AZALEAA_DB_DSN
+	
+	fmt.Printf("testvar value is %v", db_dsn)
+	db, err := sql.Open("mysql", db_dsn)
+	checkErr(err)
+
+	stmt, err := db.Prepare("INSERT INTO t_userprofile VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE value=?")
+	checkErr(err)
+
+	for _,v := range profile.Profilevals {
+		//fmt.Println(i)
+		//fmt.Println(v)
+		res, err := stmt.Exec(profile.Email, v.Key, v.Value, v.Value)
+		checkErr(err)
+	
+		affect, err := res.RowsAffected()
+		checkErr(err)
+
+		fmt.Println(affect)
+	}
+
+	db.Close()
 	return
 }
 
@@ -19,4 +76,10 @@ func main() {
 	router.HandleFunc("/profiles", CreateProfileEndpoint).Methods("POST")
 	fmt.Printf("Starting server")
 	log.Fatal(http.ListenAndServe(":80", router))
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
